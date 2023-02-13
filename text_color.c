d40e77138927226c67ccd1741c261373da73870f
minchey <98521434+minchey@users.noreply.github.com>
2023-02-13 10:42:43 +0900

    글자,배경 색입히기

---
+++ text_color.c
@@ -0,0 +1,25 @@
+#define BLACK 0 //컬러 상수지정
+#define BLUE 1 
+#define GREEN 2 
+#define CYAN 3 
+#define RED 4 
+#define MAGENTA 5 
+#define BROWN 6 
+#define LIGHTGRAY 7 
+#define DARKGRAY 8 
+#define LIGHTBLUE 9 
+#define LIGHTGREEN 10 
+#define LIGHTCYAN 11 
+#define LIGHTRED 12 
+#define LIGHTMAGENTA 13 
+#define YELLOW 14 
+#define WHITE 15 
+
+#include<stdio.h>
+#include<Windows.h>
+
+void textcolor(int foreground, int background) //글자색,배경색 순서
+{ 
+int color=foreground+background*16; 
+SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color); 
+}
